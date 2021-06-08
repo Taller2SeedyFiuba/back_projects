1eafad1f0c911d3efb5f8f258383519f4d51dcdd
JulianVentura <julianv6004@gmail.com>
2021-06-08 01:18:39 -0300

    Fix en manejo de errores y controlador de vista de proyectos

--- src/errors/handler.js
+++ src/errors/handler.js
@@ -1,47 +1,47 @@
 const { ApiError } = require("./ApiError");
 
-function msErrorHandler(err) {
+function errorResponse(res, status, error){
+  return res.status(status).json({
+    "status": "error",
+    error
+  })
+}
+
+function msErrorHandler(err, res) {
   const { response, request, message } = err;
 
   if (response) {
     console.log(response.data, response.status);
-    throw new ApiError(response.status, response.data.error);
+    return errorResponse(res, response.status, response.data.error)
   } else if (request) {
     console.log(request);
-    throw ApiError.dependencyError('back-users-req-error');
-  } else {
+    return errorResponse(res, response.status, 'internal-service-req-error')
+  } else {                                                    
     console.log('Error', message);
-    throw ApiError.dependencyError('back-users-unavailable');
+    return errorResponse(res, ApiError.codes.dependencyError, 'internal-service-req-error')
   }
 }
 
 function notDefinedHandler(req, res, next) {
   //Create error msg
-  let error = ApiError.notFound("Asked resource do not exists")
+  const error = ApiError.notFound("Asked resource do not exists")
   next(error);
 }
 
 function errorHandler(error, req, res, next) {
   if (error instanceof ApiError) {
-    return res.status(error.code).json({
-      "status": "error",
-      "message": error.message
-    })
+    return errorResponse(res, error.code, error.message)
+  }
+  if (error.isAxiosError){
+    return msErrorHandler(error, res)
   }
   if (error instanceof Error) {
     if (error.status && error.status < 500) {
-      return res.status(error.status).json({
-        "status": "error",
-        "error": error.message
-      })
+      return errorResponse(res, error.status, error.message)
     }
   }
-
   console.error("SERVER ERROR: " + error.message);
-  return res.status(500).json({
-    "status": "error",
-    "error": "internal-server-error"
-  })
+  return errorResponse(res, ApiError.codes.serverError, "internal-server-error")
 }
 
 const hocError = fn => (req, res, next) =>
@@ -53,3 +53,4 @@ module.exports = {
   errorHandler,
   hocError
 }
+
